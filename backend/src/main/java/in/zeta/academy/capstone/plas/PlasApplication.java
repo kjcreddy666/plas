c947671185908281acd767c36ce8ae7d543be220
kjcreddy666 <kappetar@zeta.tech>
2025-07-30 19:19:53 +0530

    initial commit

---
+++ backend/src/main/java/in/zeta/academy/capstone/plas/PlasApplication.java
@@ -0,0 +1,13 @@
+package in.zeta.academy.capstone.plas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/zeta/academy/capstone/plas/PlasApplicationTests.java
@@ -0,0 +1,13 @@
+package in.zeta.academy.capstone.plas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
