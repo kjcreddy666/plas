0ef529686e7232993a2da1d2f29d386dbad31eeb
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-01 19:51:25 +0530

    added find ticket by userId

--- backend/src/main/java/in/zeta/academy/capstone/plas/repository/SupportTicketRepository.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/repository/SupportTicketRepository.java
@@ -3,5 +3,10 @@ package in.zeta.academy.capstone.plas.repository;
 import in.zeta.academy.capstone.plas.entity.SupportTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
+    List<SupportTicket> findByUserId(UUID userId);
 }
