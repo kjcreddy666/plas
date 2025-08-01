0d7c7cb2f82fe906a27da09c1c88b07d4ac565ed
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-01 19:52:58 +0530

    updated ticket request dto

--- backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketRequestDto.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketRequestDto.java
@@ -1,4 +1,29 @@
 package in.zeta.academy.capstone.plas.dto;
 
+import in.zeta.academy.capstone.plas.entity.LoanApplication;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class TicketRequestDto {
+
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private Long loanApplicationId;
+
+    @NotBlank(message = "Subject is required")
+    @Size(max = 100, message = "Subject can be at most 100 characters")
+    private String subject;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 1000, message = "Description can be at most 1000 characters")
+    private String description;
+
 }
