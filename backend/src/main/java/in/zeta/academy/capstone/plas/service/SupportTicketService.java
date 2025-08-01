ebc2bf2121aa64570e6b7d9515587e28976e326c
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-01 13:47:52 +0530

    added Support Ticket Service

---
+++ backend/src/main/java/in/zeta/academy/capstone/plas/service/SupportTicketService.java
@@ -0,0 +1,11 @@
+package in.zeta.academy.capstone.plas.service;
+
+import in.zeta.academy.capstone.plas.repository.SupportTicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SupportTicketService {
+
+    @Autowired
+    private SupportTicketRepository supportTicketRepository;
+
+}
