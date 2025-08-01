e2b5fa8d03841798d20cae824c1949aa1214f0a6
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-01 14:03:44 +0530

    Modified LoanApplication entity

--- backend/src/main/java/in/zeta/academy/capstone/plas/repository/LoanApplicationRepository.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/repository/LoanApplicationRepository.java
@@ -1,4 +1,12 @@
 package in.zeta.academy.capstone.plas.repository;
-
-public class LoanApplicationRepository {
+import in.zeta.academy.capstone.plas.entity.LoanApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+@Repository
+public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
+     List<LoanApplication> findByUserIdAndApplicationDate(UUID id, LocalDate today) ;
+    List<LoanApplication> findByUserId(Long userId);
 }
