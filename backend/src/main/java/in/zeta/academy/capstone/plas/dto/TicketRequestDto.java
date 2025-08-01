84acc4cb3b0a5d85f6452dfd4d9f1ae94e257a84
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-01 19:56:14 +0530

    modified support ticket dto and repository

--- backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketRequestDto.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketRequestDto.java
@@ -1,7 +1,6 @@
 package in.zeta.academy.capstone.plas.dto;
 
-import in.zeta.academy.capstone.plas.entity.LoanApplication;
-import jakarta.validation.Valid;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
--- backend/src/main/java/in/zeta/academy/capstone/plas/repository/SupportTicketRepository.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/repository/SupportTicketRepository.java
@@ -4,7 +4,6 @@ import in.zeta.academy.capstone.plas.entity.SupportTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
