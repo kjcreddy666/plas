c679c1e0cade4d2e61e4dbc9c19a05f2158795ad
kjcreddy666 <kjcreddy666@gmail.com>
2025-08-01 11:32:40 +0530

    updated UserNotFoundException

--- backend/src/main/java/in/zeta/academy/capstone/plas/exception/UserNotFoundException.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/exception/UserNotFoundException.java
@@ -1,4 +1,7 @@
 package in.zeta.academy.capstone.plas.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
