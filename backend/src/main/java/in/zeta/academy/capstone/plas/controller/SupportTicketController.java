306cb28102b4c60dc5388e148c0dd69d6674f00a
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-01 19:54:21 +0530

    modified support ticket controller

---
+++ backend/src/main/java/in/zeta/academy/capstone/plas/controller/SupportTicketController.java
@@ -0,0 +1,39 @@
+package in.zeta.academy.capstone.plas.controller;
+
+import in.zeta.academy.capstone.plas.dto.TicketDetailsDto;
+import in.zeta.academy.capstone.plas.dto.TicketRequestDto;
+import in.zeta.academy.capstone.plas.entity.SupportTicket;
+import in.zeta.academy.capstone.plas.service.SupportTicketService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/support")
+public class SupportTicketController {
+
+    private final SupportTicketService supportTicketService;
+
+    @PostMapping("/create")
+    public ResponseEntity<SupportTicket> createTicket(@Valid @RequestBody TicketRequestDto dto) {
+        SupportTicket ticket = supportTicketService.createTicket(dto);
+        return ResponseEntity.ok(ticket);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<TicketDetailsDto>> getTicketByUserId(@PathVariable UUID userId) {
+        List<TicketDetailsDto> tickets = supportTicketService.getTicketDetailsByUserId(userId);
+        return ResponseEntity.ok(tickets);
+    }
+
+    @GetMapping("/ticket/{ticketId}")
+    public ResponseEntity<TicketDetailsDto> getTicketById(@PathVariable Long ticketId) {
+        TicketDetailsDto ticket = supportTicketService.getTicketDetailsByTicketId(ticketId);
+        return ResponseEntity.ok(ticket);
+    }
+}
