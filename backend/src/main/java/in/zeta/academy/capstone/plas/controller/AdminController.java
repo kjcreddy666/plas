d1be90e79d222d3e5c97560cee64dbef28cfe8dd
lavanyak1512 <lavanyakasina10@gmail.com>
2025-08-01 21:58:01 +0530

    updated admin controller and services

--- backend/src/main/java/in/zeta/academy/capstone/plas/controller/AdminController.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/controller/AdminController.java
@@ -1,11 +1,70 @@
 package in.zeta.academy.capstone.plas.controller;
 
+import in.zeta.academy.capstone.plas.dto.AdminLoanApplicationDto;
+import in.zeta.academy.capstone.plas.dto.AdminTicketDto;
+import in.zeta.academy.capstone.plas.dto.LoanUpdationRequestDto;
+import in.zeta.academy.capstone.plas.dto.TicketUpdationRequestDto;
+import in.zeta.academy.capstone.plas.entity.LoanApplication;
+import in.zeta.academy.capstone.plas.entity.SupportTicket;
+import in.zeta.academy.capstone.plas.enums.LoanApplicationStatus;
+import in.zeta.academy.capstone.plas.enums.TicketStatus;
+import in.zeta.academy.capstone.plas.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @RestController
-@RequestMapping("/admin/loans")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
+
+    @Autowired
+    AdminService adminService;
+
+    @GetMapping("/loans/filter")
+    public ResponseEntity<List<AdminLoanApplicationDto>> getFilteredApplications(@RequestParam List<LoanApplicationStatus> statuses) {
+        List<AdminLoanApplicationDto> filteredApplications = adminService.getFilteredLoanApplications(statuses);
+        if (filteredApplications.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(filteredApplications);
+    }
+
+    @PutMapping("/loan/{loanId}/status")
+    public ResponseEntity<HttpStatus> updateLoanStatus(@PathVariable Long loanId,
+                                                       @RequestBody LoanUpdationRequestDto request) {
+        LoanApplication updated = adminService.updateLoanStatus(loanId, request.getStatus(), request.getRemarks());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/loans")
+    public ResponseEntity<List<AdminLoanApplicationDto>> getAllLoanApplications() {
+        List<AdminLoanApplicationDto> allApplications = adminService.getAllLoanApplications();
+        if (allApplications.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(allApplications);
+    }
+
+    @GetMapping("/tickets")
+    public ResponseEntity<List<AdminTicketDto>> getSupportTickets() {
+        List<AdminTicketDto> tickets = adminService.getAllTickets();
+        if (tickets.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(tickets);
+    }
+
+    @PutMapping("/ticket/{ticketId}")
+    public ResponseEntity<HttpStatus> updateTicketStatus(@PathVariable Long ticketId,
+                                                         @RequestBody TicketUpdationRequestDto request) {
+        SupportTicket updatedTicket = adminService.updateTicketStatus(ticketId, TicketStatus.RESOLVED.name(), request.getResponse(), LocalDateTime.now());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 
 }
---
+++ backend/src/main/java/in/zeta/academy/capstone/plas/service/AdminService.java
@@ -0,0 +1,115 @@
+package in.zeta.academy.capstone.plas.service;
+
+import in.zeta.academy.capstone.plas.dto.AdminLoanApplicationDto;
+import in.zeta.academy.capstone.plas.dto.AdminTicketDto;
+import in.zeta.academy.capstone.plas.dto.LoanApplicationDto;
+import in.zeta.academy.capstone.plas.entity.LoanApplication;
+import in.zeta.academy.capstone.plas.entity.SupportTicket;
+import in.zeta.academy.capstone.plas.enums.LoanApplicationStatus;
+import in.zeta.academy.capstone.plas.enums.TicketStatus;
+import in.zeta.academy.capstone.plas.repository.LoanApplicationRepository;
+import in.zeta.academy.capstone.plas.repository.SupportTicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class AdminService {
+    @Autowired
+    LoanApplicationRepository loanApplicationRepository;
+
+    @Autowired
+    SupportTicketRepository supportTicketRepository;
+
+    public List<AdminLoanApplicationDto> getPendingLoanApplications() {
+        List<LoanApplication> loanApplications = loanApplicationRepository.findByStatusIn(
+                List.of(LoanApplicationStatus.NEW.name(), LoanApplicationStatus.UNDER_REVIEW.name()));
+        return loanApplications.stream()
+                .map(loan -> new AdminLoanApplicationDto(
+                        loan.getId(),
+                        loan.getUser().getId(),
+                        loan.getAmount(),
+                        loan.getTenureMonths(),
+                        loan.getIncome(),
+                        loan.getCreditScore(),
+                        loan.getStatus(),
+                        loan.getApplicationDate(),
+                        loan.getPurpose(),
+                        loan.getReviewRemarks()))
+                .toList();
+    }
+
+    public List<AdminLoanApplicationDto> getFilteredLoanApplications(List<LoanApplicationStatus> statuses) {
+        List<LoanApplication> loanApplications =  loanApplicationRepository.findByStatusIn(
+                statuses.stream().map(LoanApplicationStatus::name).toList());
+        return loanApplications.stream()
+                .map(loan -> new AdminLoanApplicationDto(
+                        loan.getId(),
+                        loan.getUser().getId(),
+                        loan.getAmount(),
+                        loan.getTenureMonths(),
+                        loan.getIncome(),
+                        loan.getCreditScore(),
+                        loan.getStatus(),
+                        loan.getApplicationDate(),
+                        loan.getPurpose(),
+                        loan.getReviewRemarks()))
+                .toList();
+    }
+
+    public LoanApplication updateLoanStatus(Long loanId, LoanApplicationStatus status, String remarks) {
+        LoanApplication loanApplication = loanApplicationRepository.findById(loanId)
+                .orElseThrow(() -> new RuntimeException("Loan application not found"));
+
+        loanApplication.setStatus(status);
+        loanApplication.setReviewRemarks(remarks);
+
+        return loanApplicationRepository.save(loanApplication);
+    }
+
+    public List<AdminLoanApplicationDto> getAllLoanApplications() {
+        List<LoanApplication> loanApplications = loanApplicationRepository.findAll();
+        return loanApplications.stream()
+                .map(loan -> new AdminLoanApplicationDto(
+                        loan.getId(),
+                        loan.getUser().getId(),
+                        loan.getAmount(),
+                        loan.getTenureMonths(),
+                        loan.getIncome(),
+                        loan.getCreditScore(),
+                        loan.getStatus(),
+                        loan.getApplicationDate(),
+                        loan.getPurpose(),
+                        loan.getReviewRemarks()))
+                .toList();
+    }
+
+    public List<AdminTicketDto> getAllTickets() {
+        List<SupportTicket> tickets = supportTicketRepository.findAll();
+        return tickets.stream()
+                .map(ticket -> new AdminTicketDto(
+                        ticket.getId(),
+                        ticket.getUser().getId(),
+                        ticket.getLoanApplication() != null ? ticket.getLoanApplication().getId() : null,
+                        ticket.getSubject(),
+                        ticket.getDescription(),
+                        ticket.getStatus(),
+                        ticket.getCreatedAt(),
+                        ticket.getUpdatedAt(),
+                        ticket.getResponse()))
+                .toList();
+    }
+
+    public SupportTicket updateTicketStatus(Long ticketId, String status, String response, LocalDateTime now) {
+        SupportTicket ticket = supportTicketRepository.findById(ticketId)
+                .orElseThrow(() -> new RuntimeException("Support ticket not found"));
+
+        ticket.setStatus(TicketStatus.valueOf(status));
+        ticket.setResponse(response);
+        ticket.setUpdatedAt(now);
+
+        return supportTicketRepository.save(ticket);
+    }
+}
