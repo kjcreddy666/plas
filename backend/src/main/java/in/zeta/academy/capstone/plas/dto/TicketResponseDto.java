1cf98da2dd8836c0adae46fc0c6824dd0d390f8b
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-04 15:00:30 +0530

    modified ticket response dto

--- backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketResponseDto.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/dto/TicketResponseDto.java
@@ -3,13 +3,16 @@ package in.zeta.academy.capstone.plas.dto;
 import in.zeta.academy.capstone.plas.enums.TicketStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TicketResponseDto {
-    private Long ticketId;
+    private Long Id;
     private Long loanId;
     private String subject;
     private String description;
