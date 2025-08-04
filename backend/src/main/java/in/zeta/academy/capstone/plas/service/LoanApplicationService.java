2bf719c49e8499733f74028aef0b5edf4487e202
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-04 11:50:11 +0530

    Validated reviewedBy field Attribute

--- backend/src/main/java/in/zeta/academy/capstone/plas/service/LoanApplicationService.java
+++ backend/src/main/java/in/zeta/academy/capstone/plas/service/LoanApplicationService.java
@@ -81,7 +81,7 @@ public class LoanApplicationService {
                 .status(loan.getStatus().toString())
                 .applicationDate(loan.getApplicationDate())
                 .purpose(loan.getPurpose())
-                .reviewedBy(String.valueOf(loan.getUser().getRole()))
+                .reviewedBy("ADMIN")
                 .reviewedAt(loan.getReviewedAt())
                 .reviewRemarks(loan.getReviewRemarks())
                 .build();
