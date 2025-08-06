d1a156f473ba20c02ff1f9d91c07311281945d18
lavanyak1512 <lavanyakasina10@gmail.com>
2025-08-06 23:26:42 +0530

    added Sidebar.vue

---
+++ frontend/src/components/dashboard/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <QuickActions :actions="quickActions" />
+      <RecentSupport :tickets="supportTickets" />
+    </div>
+  </template>
+  
+  <script setup>
+  import QuickActions from './QuickActions.vue';
+  import RecentSupport from './RecentSupport.vue';
+  
+  defineProps({
+    quickActions: Array,
+    supportTickets: Array,
+  });
+  </script>
+  
