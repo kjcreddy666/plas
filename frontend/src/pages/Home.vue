97451aeb0f903cce5d5b24ed2ac3cec6a4920204
Naveen <naveenjinkala33@gmail.com>
2025-08-07 00:32:11 +0530

    Add Home.vue

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <FinancialFuture />
+    <FeatureSection />
+</template>
+<script setup>
+import FinancialFuture from '@/components/home/FinancialFuture.vue';
+import FeatureSection from '@/components/home/FeatureSection.vue';
+</script>
