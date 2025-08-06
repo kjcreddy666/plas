fcc38e8f91f41b4e798c76c13477395be40a3e8d
lavanyak1512 <lavanyakasina10@gmail.com>
2025-08-06 21:32:28 +0530

    added StatsRow component to display a list of statistics

---
+++ frontend/src/components/dashboard/StatsRow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row g-3 mb-4">
+      <StatCard
+        v-for="stat in stats"
+        :key="stat.label"
+        :label="stat.label"
+        :value="stat.value"
+        :cssClass="stat.class"
+      />
+    </div>
+</template>
+  
+<script setup>
+  import StatCard from './StatCard.vue';
+  defineProps({ stats: Array });
+</script>
