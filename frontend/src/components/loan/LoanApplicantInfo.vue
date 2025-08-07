ab2dabced9c12e8a3b2f03c0ed0220216e1cfa04
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-07 09:50:00 +0530

    Added LoanApplicantInfo component for displaying applicant information

---
+++ frontend/src/components/loan/LoanApplicantInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-body">
+      <h5><i class="bi bi-person-fill"></i> Applicant Information</h5>
+      <p><strong>Full Name: </strong> {{ userName }}</p>
+      <p><strong>Email Address: </strong> {{ userEmail }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  loan: Object
+});
+
+const userName = localStorage.getItem('userName');
+const userEmail = localStorage.getItem('userEmail');
+</script>
