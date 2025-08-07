817c07ab1c4dd170dffd1987e01db3f5dfc77b0b
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-07 09:47:42 +0530

    Added LoanStepIndicator component for visualizing loan application steps

---
+++ frontend/src/components/apply/LoanStepIndicator.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="d-flex justify-content-center mb-4">
+      <template v-for="(stepLabel, index) in steps" :key="index">
+        <div class="text-center mx-2">
+          <div
+            :class="[
+              'rounded-circle p-2',
+              currentStep > index
+                ? 'bg-success text-white'
+                : currentStep === index
+                ? 'bg-primary text-white'
+                : 'bg-light',
+            ]"
+            style="width: 40px; height: 40px"
+          >
+            {{ index + 1 }}
+          </div>
+          <small>{{ stepLabel }}</small>
+        </div>
+        <div
+          v-if="index < steps.length - 1"
+          class="align-self-center mx-2 border-top border-2"
+          style="width: 40px"
+        ></div>
+      </template>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['steps', 'currentStep']
+  }
+  </script>
+  
