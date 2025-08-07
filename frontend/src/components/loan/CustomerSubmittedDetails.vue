bb1d75e6b92a6a878950782da33fb8b6c83bd86c
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-07 09:52:16 +0530

    Added CustomerSubmittedDetails component for displaying submitted loan details

---
+++ frontend/src/components/loan/CustomerSubmittedDetails.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card mb-4">
+      <div class="card-body">
+        <h5><i class="bi bi-file-earmark-text"></i> Customer Submitted Details</h5>
+        <p><strong>Income:</strong> ${{ loan.income }}</p>
+        <p><strong>Credit Score:</strong> {{ loan.creditScore }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps(['loan']);
+  </script>
+  
