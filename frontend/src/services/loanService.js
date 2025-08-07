a8c35859cfd8fef8802745346f66c490ab55be2f
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-07 09:48:32 +0530

    Added loanService module for handling loan application requests

---
+++ frontend/src/services/loanService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { handleRequest } from './requestHandler';
+import { getAuthHeaders } from './authHeaders';
+
+const API_URL = 'http://localhost:8080/loans';
+
+export const applyForLoan = async (data) =>
+  handleRequest(
+    () => axios.post(`${API_URL}/apply`, data, getAuthHeaders()),
+    'Loan application submitted successfully.'
+  );
+
+export const getApplicationsByUser = async (userId) =>
+  handleRequest(
+    () => axios.get(`${API_URL}/user/${userId}`, getAuthHeaders()),
+    'Applications fetched successfully.'
+  );
+
+export const getApplicationById = async (loanId) =>
+  handleRequest(
+    () => axios.get(`${API_URL}/${loanId}`, getAuthHeaders()),
+    'Application fetched successfully.'
+  );
