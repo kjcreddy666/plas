9e5015b0633e3d1a321d72476943698d6570a2be
KavyaGaja <kavyagaja3151@gmail.com>
2025-08-07 11:42:25 +0530

    added recent support.vue

---
+++ frontend/src/components/dashboard/RecentSupport.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="border rounded-4 p-3 shadow-sm">
+      <h5 class="fw-bold">Recent Support</h5>
+      <div class="mt-3">
+        <SupportTicket
+          v-for="ticket in tickets"
+          :key="ticket.subject"
+          v-bind="ticket"
+          @click="$router.push(`/ticket/${ticket.ticketId}`)"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import SupportTicket from './SupportTicket.vue';
+  defineProps({ tickets: Array });
+  </script>
+  
