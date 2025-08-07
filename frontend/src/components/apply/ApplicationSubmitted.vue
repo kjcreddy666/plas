b63db179c6aad0d0c78e999abf900f51e2fe9ac0
keerthitalakoti <keerthitalakoti03@gmail.com>
2025-08-07 09:55:25 +0530

    Added ApplicationSubmitted component to loan application successfull message

---
+++ frontend/src/components/apply/ApplicationSubmitted.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-center mx-auto w-50">
+      <div class="alert alert-success">
+        <h4>🎉 Application Submitted Successfully!</h4>
+        <p>Thank you, <strong>{{ name }}</strong>. We’ve received your application.</p>
+      </div>
+      <div class="d-flex justify-content-center gap-3 mt-4">
+        <button class="btn btn-outline-primary" @click="goHome">Home</button>
+        <button class="btn btn-primary" @click="goDashboard">Dashboard</button>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    props: ['name'],
+    methods: {
+      goHome() {
+        this.$router.push('/');
+      },
+      goDashboard() {
+        this.$router.push('/dashboard');
+      }
+    }
+  }
+  </script>
