dc58ef28eaae31cb067dd25a58b2ce2d1459e2dd
Naveen <naveenjinkala33@gmail.com>
2025-08-08 15:40:03 +0530

    Update request handler with improved API communication and error management

--- frontend/src/services/requestHandler.js
+++ frontend/src/services/requestHandler.js
@@ -1,16 +1,27 @@
 export const handleRequest = async (requestFn, successMessage) => {
-    try {
-      const response = await requestFn();
+  try {
+    const response = await requestFn();
+
+    // Handle 204 No Content
+    if (response.status === 204) {
       return {
         isSuccess: true,
-        message: successMessage,
-        data: response.data,
-      };
-    } catch (error) {
-      console.log(error)
-      return {
-        isSuccess: false,
-        message: error.response?.data?.message || 'Something went wrong.',
+        message: 'No content available.',
+        data: null,
       };
     }
+
+    return {
+      isSuccess: true,
+      message: successMessage,
+      data: response.data,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      isSuccess: false,
+      message: error.response?.data?.message || 'Something went wrong.',
+      data: null,
+    };
+  }
 };
