6e30e7dba12ab90828c795da75926ba684efae15
gaja_kavya <gajak@zeta.tech>
2025-08-08 15:00:02 +0530

    updated recent support for showing recent tickets

--- frontend/src/components/dashboard/RecentSupport.vue
+++ frontend/src/components/dashboard/RecentSupport.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="border rounded-4 p-3 shadow-sm">
+  <div class="border rounded-4 p-3 shadow-sm">
+    <div class="d-flex justify-content-between align-items-center">
       <h5 class="fw-bold">Recent Support</h5>
-      <div class="mt-3">
-        <SupportTicket
-          v-for="ticket in tickets"
-          :key="ticket.subject"
-          v-bind="ticket"
-          @click="$router.push(`/ticket/${ticket.ticketId}`)"
-        />
-      </div>
+      <router-link
+        v-if="tickets.length > 2"
+        to="/support-ticket"
+        class="btn btn-sm btn-link"
+      >
+        View More
+      </router-link>
     </div>
-  </template>
-  
-  <script setup>
-  import SupportTicket from './SupportTicket.vue';
-  defineProps({ tickets: Array });
-  </script>
-  
+
+    <div class="mt-3">
+      <SupportTicket
+      class="cursor-pointer"
+        v-for="ticket in limitedTickets"
+        :key="ticket.subject"
+        v-bind="ticket"
+        @click="$router.push(`/ticket/${ticket.ticketId}`)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SupportTicket from './SupportTicket.vue';
+import { computed } from 'vue';
+const props = defineProps({ tickets: Array });
+
+// Show only first 2 tickets
+const limitedTickets = computed(() => props.tickets.slice(0, 2));
+</script>
