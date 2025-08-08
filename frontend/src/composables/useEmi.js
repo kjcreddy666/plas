3f70b743aa17f5839ec93af4dc3f3b180128f7cf
Naveen <naveenjinkala33@gmail.com>
2025-08-08 15:40:03 +0530

    Update EMI composable with enhanced calculation logic and validation

--- frontend/src/composables/useEmi.js
+++ frontend/src/composables/useEmi.js
@@ -1,4 +1,3 @@
-// composables/useEmi.js
 import { ref } from 'vue';
 import { useApi } from './useApi';
 import {
@@ -9,6 +8,10 @@ import {
 export const useEmi = () => {
   const emiData = ref(null);
   const schedule = ref([]);
+  const totalItems = ref(0);
+  const currentPage = ref(0);
+  const pageSize = ref(12);
+
   const { loading, error, execute } = useApi();
 
   const calculatePreview = async (formData) => {
@@ -16,14 +19,24 @@ export const useEmi = () => {
     if (response.isSuccess) emiData.value = response.data;
   };
 
-  const fetchRepaymentSchedule = async (loanId, annualRate) => {
-    const response = await execute(() => generateRepaymentSchedule(loanId, annualRate));
-    if (response.isSuccess) schedule.value = response.data;
+  const fetchRepaymentSchedule = async (loanId, annualRate, page = 0, size = 12) => {
+    const response = await execute(() =>
+      generateRepaymentSchedule(loanId, annualRate, page, size)
+    );
+    if (response.isSuccess) {
+      schedule.value = response.data.content;
+      totalItems.value = response.data.totalElements;
+      currentPage.value = response.data.page;
+      pageSize.value = response.data.size;
+    }
   };
 
   return {
     emiData,
     schedule,
+    totalItems,
+    currentPage,
+    pageSize,
     error,
     loading,
     calculatePreview,
