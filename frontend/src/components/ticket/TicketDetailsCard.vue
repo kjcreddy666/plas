11a443e2b881e938dbfdcedb652ff9af1ff863fd
lavanyak1512 <lavanyakasina10@gmail.com>
2025-08-08 14:52:13 +0530

    updated TicketDetailsCard.vue

--- frontend/src/components/ticket/TicketDetailsCard.vue
+++ frontend/src/components/ticket/TicketDetailsCard.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="card mb-4">
     <div class="card-body">
-      <h5><i class="bi bi-currency-dollar"></i> Ticket Details</h5>
+      <h5><strong>Ticket Details</strong></h5>
+
       <p><strong>Ticket ID:</strong> {{ ticket?.id }}</p>
-      
+
       <p>
         <strong>Linked Loan ID:</strong>
         <router-link :to="`/loans/${ticket?.loanId}`" class="text-decoration-none">
@@ -14,7 +15,7 @@
       <p><strong>Subject:</strong> {{ ticket?.subject }}</p>
       <p><strong>Description:</strong> {{ ticket?.description }}</p>
       <p><strong>Created At:</strong> {{ new Date(ticket?.createdAt).toLocaleString() }}</p>
-      <p><strong>Last Updated:</strong> {{ new Date(ticket?.updatedAt).toLocaleString() }}</p>
+      <p v-if="ticket?.status !== 'OPEN'"><strong>Last Updated:</strong> {{ new Date(ticket?.updatedAt).toLocaleString()}}</p>
     </div>
   </div>
 </template>
